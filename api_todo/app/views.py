4e38ba242acd258ca473e8b15a5178148ab4468c
Franklin Angelo Arenas <franklin.silva8@fatec.sp.gov.br>
2021-11-01 14:00:23 -0300

    criado serializers para serialização do banco'

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from api_todo.app.serializers import TodoSerializer
+from app.models import Todo
+from app.serializers import TodoSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def todo_list(request):
+    todo = Todo.objects.all()
+    serializer =TodoSerializer(todo, many=True)
+    return Response(serializer.data)
